a337c5ebf9eac000c90b6d85e6d73850d83a2329
GromovBoris <drunkgonzo@gmail.com>
2024-12-09 16:33:47 +0300

    feat: profile add

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classnames from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -6,11 +6,13 @@ import {
   faAngleRight,
   faAngleUp,
   faAngleDown,
+  faSignOutAlt,
 } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../assets/logo.png";
 import user from "../../assets/jimmy.png";
 import PropTypes from "prop-types";
 import {
+  Wrapper,
   Container,
   Logo,
   LogoWrapper,
@@ -23,6 +25,13 @@ import {
   ProfWrapper,
   ProfInfo,
   InfoWrapper,
+  InfoBlock,
+  ProfItem,
+  BlockInfo,
+  ProfBottom,
+  Logout,
+  LogoutItem,
+  ProfBar,
 } from "./styles";
 
 const routes = [
@@ -34,6 +43,12 @@ const routes = [
   { title: "Messages", icon: "envelope", path: "/messages" },
 ];
 
+const profRoutes = [
+  { title: "Profile", path: "/profile" },
+  { title: "Manage", path: "/manage" },
+  { title: "History", path: "/history" },
+];
+
 const bottomRoutes = [
   { title: "Settings", icon: "sliders", path: "/settings" },
   { title: "Support", icon: "phone-volume", path: "/support" },
@@ -42,11 +57,16 @@ const bottomRoutes = [
 const Sidebar = (props) => {
   const { color } = props;
   const [isOpened, setIsOpened] = useState(true);
-  const [isShows, setIsShows] = useState(false);
-  const containerClassnames = classnames({ opened: isOpened });
+  const [isActive, setIsActive] = useState(null);
+  const [showProfile, setShowProfile] = useState(false);
+
+  const containerClassnames = classnames({
+    shows: showProfile,
+    opened: isOpened,
+  });
 
   const goToRoute = (path) => {
-    console.log(`going to "${path}"`);
+    setIsActive((prev) => (prev === path ? null : path));
   };
 
   const toggleSidebar = () => {
@@ -54,61 +74,124 @@ const Sidebar = (props) => {
   };
 
   const toggleProfile = () => {
-    setIsShows(!isShows);
+    setShowProfile(!showProfile);
   };
 
+  const handleClick = () => {
+    setIsActive((prev) => !prev);
+  };
+
+  const handleOutsideClick = (e) => {
+    if (!e.target.closest(".active")) {
+      setIsActive(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
   return (
-    <Container isOpened={isOpened} className={containerClassnames}>
-      <Logo>
-        <LogoWrapper isOpened={isOpened}>
-          <img src={logo} alt="TensorFlow logo" />
-          <span>TensorFlow</span>
-        </LogoWrapper>
-        <IconWrapper onClick={toggleSidebar} isOpened={isOpened}>
-          <FontAwesomeIcon icon={isOpened ? faAngleLeft : faAngleRight} />
-        </IconWrapper>
-      </Logo>
-      <Nav>
-        {routes.map((route) => (
-          <NavItem
-            key={route.title}
-            onClick={() => {
-              goToRoute(route.path);
-            }}
-            isOpened={isOpened}
-          >
-            <FontAwesomeIcon icon={route.icon} />
-            <span>{route.title}</span>
-          </NavItem>
-        ))}
-      </Nav>
-      <Sup>
-        {bottomRoutes.map((route) => (
-          <SupItem
-            key={route.title}
-            onClick={() => {
-              goToRoute(route.path);
-            }}
-            isOpened={isOpened}
-          >
-            <FontAwesomeIcon icon={route.icon} />
-            <span>{route.title}</span>
-          </SupItem>
-        ))}
-      </Sup>
-      <Prof isOpened={isOpened}>
+    <Wrapper color={color}>
+      <Container
+        color={color}
+        isOpened={isOpened}
+        className={containerClassnames}
+      >
+        <Logo>
+          <LogoWrapper isOpened={isOpened}>
+            <img src={logo} alt="TensorFlow logo" />
+            <span>TensorFlow</span>
+          </LogoWrapper>
+          <IconWrapper onClick={toggleSidebar} isOpened={isOpened}>
+            <FontAwesomeIcon icon={isOpened ? faAngleLeft : faAngleRight} />
+          </IconWrapper>
+        </Logo>
+        <Nav>
+          {routes.map((route) => (
+            <NavItem
+              className={isActive === route.path ? "active" : ""}
+              key={route.title}
+              onClick={() => {
+                handleClick();
+                goToRoute(route.path);
+              }}
+              isOpened={isOpened}
+              color={color}
+            >
+              <FontAwesomeIcon icon={route.icon} />
+              <span>{route.title}</span>
+            </NavItem>
+          ))}
+        </Nav>
+        <Sup>
+          {bottomRoutes.map((route) => (
+            <SupItem
+              key={route.title}
+              onClick={() => {
+                goToRoute(route.path);
+              }}
+              color={color}
+              isOpened={isOpened}
+              className={isActive === route.path ? "active" : ""}
+            >
+              <FontAwesomeIcon icon={route.icon} />
+              <span>{route.title}</span>
+            </SupItem>
+          ))}
+        </Sup>
+        <Prof isOpened={isOpened} color={color} showProfile={showProfile}>
+          <ProfWrapper isOpened={isOpened}>
+            <img src={user} alt="user logo" />
+            <ProfInfo isOpened={isOpened}>
+              <p>User Account</p>
+              <h2>Boris G.</h2>
+            </ProfInfo>
+          </ProfWrapper>
+          <InfoWrapper onClick={toggleProfile} isOpened={isOpened}>
+            <FontAwesomeIcon icon={showProfile ? faAngleDown : faAngleUp} />
+          </InfoWrapper>
+        </Prof>
+      </Container>{" "}
+      <InfoBlock color={color} showProfile={showProfile}>
         <ProfWrapper isOpened={isOpened}>
           <img src={user} alt="user logo" />
-          <ProfInfo isOpened={isOpened}>
-            <p>User Account</p>
-            <h2>Boris G.</h2>
-          </ProfInfo>
-        </ProfWrapper>
-        <InfoWrapper onClick={toggleProfile} isOpened={isOpened}>
-          <FontAwesomeIcon icon={isShows ? faAngleDown : faAngleUp} />
-        </InfoWrapper>
-      </Prof>
-    </Container>
+          <BlockInfo>
+            <h2>Boris Gromov</h2>
+            <p>cv@gromovboris.com</p>
+          </BlockInfo>
+        </ProfWrapper>{" "}
+        <ProfBar>
+          {profRoutes.map((route) => (
+            <ProfItem
+              key={route.title}
+              onClick={() => {
+                goToRoute(route.path);
+              }}
+              color={color}
+              isOpened={isOpened}
+              className={isActive === route.path ? "active" : ""}
+            >
+              <FontAwesomeIcon icon={route.icon} />
+              <span>{route.title}</span>
+            </ProfItem>
+          ))}
+        </ProfBar>
+        <ProfBottom>
+          <Logout>
+            <LogoutItem>Logout</LogoutItem>
+            <FontAwesomeIcon icon={faSignOutAlt} />
+          </Logout>
+          <p>
+            V10.30 - <span>Terms and Conditions</span>
+          </p>
+        </ProfBottom>
+      </InfoBlock>
+    </Wrapper>
   );
 };
 
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -1,15 +1,29 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+  height: 100%;
+`;
+
 export const Container = styled.div`
-  background-color: var(--color-sidebar-background-light-default);
-  color: var(--color-text-light-default);
   width: ${(props) => (props.isOpened ? "250px" : "100px")};
   display: flex;
   flex-direction: column;
   border: 3px double #f0f2ff;
   border-radius: 10px;
   transition: width 0.4s ease, transform 0.4s ease;
-  margin: 20px;
+  margin: 20px 20px 0px 20px;
+  // overflow: hidden;
+  background-color: ${(props) =>
+    props.color === "dark"
+      ? "var(--color-sidebar-background-dark-default)"
+      : "var(--color-sidebar-background-light-default)"};
+
+  color: ${(props) =>
+    props.color === "dark"
+      ? "var(--color-text-dark-default)"
+      : "var(--color-text-light-default)"};
 `;
 
 export const Logo = styled.div`
@@ -22,6 +36,7 @@ export const LogoWrapper = styled.div`
   border-radius: 10px;
   align-items: center;
   margin: 15px;
+  cursor: default;
 
   img {
     width: 40px;
@@ -36,9 +51,7 @@ export const LogoWrapper = styled.div`
     font-size: 1.2em;
     white-space: nowrap;
     overflow: hidden;
-    transition: opacity $transition-duration ease;
     opacity: ${(props) => (props.isOpened ? "1" : "0")};
-    transition: opacity $transition-duration ease;
     transition: opacity 0.5s ease, transform 0.5s ease;
     transform: ${(props) =>
       props.isOpened
@@ -62,8 +75,16 @@ export const IconWrapper = styled.div`
   transition: transform 0.8s ease, background-color 0.8s ease,
     margin-left 0.5s ease;
 
+  background-color: ${(props) =>
+    props.color === "dark"
+      ? "var(--color-button-background-dark-active)"
+      : "var(--color-button-background-light-active)"};
+
   &:hover {
-    background-color: var(--color-button-background-dark-active);
+    background-color: ${(props) =>
+      props.color === "dark"
+        ? "var(--color-button-background-dark-active)"
+        : "var(--color-button-background-light-hover)"};
   }
 `;
 
@@ -77,18 +98,43 @@ export const NavItem = styled.div`
   display: flex;
   align-items: center;
   margin-left: 10px;
+  margin-right: 10px;
   padding: 10px;
   cursor: pointer;
   border-radius: 10px;
   padding-left: 20px;
   transition: background-color 0.5s ease, padding-left 0.5s ease;
+  background-color: ${(props) =>
+    props.color === "dark"
+      ? "var(--color-button-background-dark-default)"
+      : "var(--color-button-background-light-default)"};
 
   &:hover {
-    background-color: var(--color-sidebar-background-light-hover);
+    background-color: ${(props) =>
+      props.color === "dark"
+        ? "var(--color-sidebar-background-dark-hover)"
+        : "var(--color-sidebar-background-light-hover)"};
+
+    span {
+      color: ${(props) =>
+        props.color === "dark"
+          ? "var(--color-text-dark-hover)"
+          : "var(--color-text-light-hover)"};
+    }
   }
 
   &.active {
-    background-color: var(--color-sidebar-background-light-active);
+    background-color: ${(props) =>
+      props.color === "dark"
+        ? "var(--color-button-background-dark-active)"
+        : "var(--color-button-background-light-active)"};
+
+    span {
+      color: ${(props) =>
+        props.color === "dark"
+          ? "var(--color-text-dark-active)"
+          : "var(--color-text-light-active)"};
+    }
   }
 
   svg {
@@ -100,7 +146,6 @@ export const NavItem = styled.div`
   span {
     margin-left: 10px;
     color: var(--color-text-light-default);
-    transition: opacity $transition-duration ease;
     transition: opacity 0.5s ease, transform 0.5s ease;
     opacity: ${(props) => (props.isOpened ? "1" : "0")};
     transform: ${(props) =>
@@ -114,15 +159,82 @@ export const Sup = styled(Nav)`
   margin-top: 50px;
   margin-bottom: 20px;
 `;
+
 export const SupItem = NavItem;
+export const ProfItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+  margin-right: 10px;
+  padding: 5px;
+  cursor: pointer;
+  border-radius: 10px;
+  padding-left: 20px;
+  transition: background-color 0.5s ease, padding-left 0.5s ease;
+  background-color: ${(props) =>
+    props.color === "dark"
+      ? "var(--color-button-background-dark-default)"
+      : "var(--color-button-background-light-default)"};
+
+  &:hover {
+    background-color: ${(props) =>
+      props.color === "dark"
+        ? "var(--color-sidebar-background-dark-hover)"
+        : "var(--color-sidebar-background-light-hover)"};
+
+    span {
+      color: ${(props) =>
+        props.color === "dark"
+          ? "var(--color-text-dark-hover)"
+          : "var(--color-text-light-hover)"};
+    }
+  }
+
+  &.active {
+    background-color: ${(props) =>
+      props.color === "dark"
+        ? "var(--color-button-background-dark-active)"
+        : "var(--color-button-background-light-active)"};
+
+    span {
+      color: ${(props) =>
+        props.color === "dark"
+          ? "var(--color-text-dark-active)"
+          : "var(--color-text-light-active)"};
+    }
+  }
+
+  span {
+    color: var(--color-text-light-default);
+  }
+`;
 
 export const Prof = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
-  margin-left: 10px;
+  padding-left: 10px;
   max-height: 70px;
-  // overflow: hidden;
+  position: relative;
+  border-radius: 0 0 7px 7px;
+  background-color: ${(props) =>
+    props.color === "dark"
+      ? props.showProfile && !props.isOpened
+        ? "var(--color-text-light-active)"
+        : "var(--color-sidebar-background-dark-default)"
+      : props.showProfile && !props.isOpened
+      ? "var(--color-text-logo-light-default)"
+      : "var(--color-button-background-light-default)"};
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: -2px;
+    right: -2px;
+    height: 1px;
+    background-color: #e2e8f0;
+  }
 `;
 
 export const ProfWrapper = styled.div`
@@ -133,13 +245,15 @@ export const ProfWrapper = styled.div`
   justify-content: space-between;
   margin: 15px;
   flex-shrink: 0;
+  cursor: default;
 
   img {
     width: 40px;
     height: auto;
+    margin-left: ${(props) => (props.isOpened ? "0px" : "5px")};
     margin-right: 10px;
     border-radius: 10px;
-    transition: transform 0.8s ease;
+    transition: transform 0.8s ease, margin-left 0.8s ease;
     transform: ${(props) => (props.isOpened ? "scale(1)" : "scale(1.2)")};
   }
 `;
@@ -148,7 +262,6 @@ export const ProfInfo = styled.div`
   flex-direction: column;
   font-size: 0.7em;
   margin-left: 10px;
-  // margin-left: ${(props) => (props.isOpened ? "10px" : "0")};
   transition: opacity 0.5s ease, transform 0.5s ease;
   opacity: ${(props) => (props.isOpened ? "1" : "0")};
   transform: ${(props) =>
@@ -171,7 +284,7 @@ export const InfoWrapper = styled.div`
   background-color: var(--color-button-background-light-active);
   margin-top: 5px;
   cursor: pointer;
-  margin-left: ${(props) => (props.isOpened ? "78px" : "-74px")};
+  margin-left: ${(props) => (props.isOpened ? "76px" : "-79px")};
 
   transition: transform 0.8s ease, background-color 0.8s ease,
     margin-left 0.5s ease;
@@ -179,4 +292,79 @@ export const InfoWrapper = styled.div`
   &:hover {
     background-color: var(--color-button-background-dark-active);
   }
+`;
+
+export const InfoBlock = styled.div`
+  border: 3px double #f0f2ff;
+  border-radius: 10px;
+  margin: 20px 0px 0px 0px;
+  width: 200px;
+  max-height: 300px;
+  opacity: ${({ showProfile }) => (showProfile ? "1" : "0")};
+  transform: translateY(${({ showProfile }) => (showProfile ? "0" : "50px")});
+  transition: opacity 0.5s ease, transform 0.5s ease;
+  background-color: ${(props) =>
+    props.color === "dark"
+      ? "var(--color-sidebar-background-dark-default)"
+      : "var(--color-sidebar-background-light-default)"};
+
+  color: ${(props) =>
+    props.color === "dark"
+      ? "var(--color-text-dark-default)"
+      : "var(--color-text-light-default)"};
+`;
+
+export const BlockInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 0.7em;
+  margin-left: 10px;
+
+  p,
+  h2 {
+    margin: 0;
+    flex: 1;
+  }
+`;
+
+export const ProfBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  max-height: 70px;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: -2px;
+    right: -2px;
+    height: 1px;
+    background-color: #e2e8f0;
+  }
+
+  p {
+    font-size: 10px;
+
+    span {
+      text-decoration: underline;
+    }
+  }
+`;
+export const Logout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  cursor: pointer;
+  padding: 10px;
+`;
+export const LogoutItem = styled.div``;
+
+export const ProfBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0px;
 `;
