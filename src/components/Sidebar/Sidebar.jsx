a7c35d25e516c63348daa4b8e2fd6b624e3948ed
yssfrvnkm <ilya.frontman@yandex.ru>
2025-07-13 02:57:55 +0300

    udpate Sidebar: * add styled components into index; * include styles into Sidebar.jsx (change tags to components); * edit routes data (active page) for demo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,9 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../../assets/logo.png';
 import PropTypes from 'prop-types';
 
+import { HeaderSection, ImgLogo, LinkDesc, LinkInnerBox, LogoDesc, NavBottomContainer, NavContainer, SidebarContainer, ToggleSidebarButton } from '.';
+
+
 const routes = [
     { title: 'Home', icon: 'fas-solid fa-house', path: '/' },
-    { title: 'Sales', icon: 'chart-line', path: '/sales' },
+    { title: 'Sales', icon: 'chart-line', path: '/sales', active: true}, // react-router current active page
     { title: 'Costs', icon: 'chart-column', path: '/costs' },
     { title: 'Payments', icon: 'wallet', path: '/payments' },
     { title: 'Finances', icon: 'chart-pie', path: '/finances' },
@@ -32,45 +35,51 @@ const Sidebar = (props) => {
     };
 
     return (
-        <div className={ containerClassnames }>
-            <div>
-                <img src={ logo } alt="TensorFlow logo"/>
-                <span>TensorFlow</span>
-                <div onClick={ toggleSidebar }>
-                    <FontAwesomeIcon icon={ isOpened ? 'angle-left' : 'angle-right' }/>
-                </div>
-            </div>
-            <div>
+        <SidebarContainer className={ containerClassnames } $color={color} $isOpened={isOpened}>
+            <HeaderSection>
+                <ImgLogo src={ logo } alt="TensorFlow logo"/>
+                <LogoDesc $color={color} $show={isOpened}>TensorFlow</LogoDesc>
+                <ToggleSidebarButton $color={color} onClick={ toggleSidebar } $isOpened={isOpened}>
+                    <FontAwesomeIcon icon={ isOpened ? 'angle-left' : 'angle-right' } />
+                </ToggleSidebarButton>
+            </HeaderSection>
+            <NavContainer id='main'>
                 {
                     routes.map(route => (
-                        <div
+                        <LinkInnerBox
                             key={ route.title }
                             onClick={() => {
                                 goToRoute(route.path);
                             }}
+                            $color={color}
+                            $isOpened={isOpened}
+                            className={route?.active ? 'active': ''}
                         >
-                            <FontAwesomeIcon icon={ route.icon }/>
-                            <span>{ route.title }</span>
-                        </div>
+                            <FontAwesomeIcon icon={ route.icon } />
+                            <LinkDesc $color={color} $show={isOpened} >{ route.title }</LinkDesc>
+                        </LinkInnerBox>
                     ))
                 }
-            </div>
-            <div>
+            </NavContainer>
+            <NavBottomContainer id='bottom'>
                 {
                     bottomRoutes.map(route => (
-                        <div
+                        <LinkInnerBox
                             key={ route.title }
                             onClick={() => {
                                 goToRoute(route.path);
                             }}
+                            $color={color}
+                            $isOpened={isOpened}
+                            /* className='active' */
                         >
-                            <FontAwesomeIcon icon={ route.icon }/>
-                            <span>{ route.title }</span>
-                        </div>
+                            <FontAwesomeIcon icon={ route.icon } />
+                            <LinkDesc $color={color} $show={isOpened}>{ route.title }</LinkDesc>
+                        </LinkInnerBox>
                     ))
                 }
-            </div>
-        </div>
+            </NavBottomContainer>
+        </SidebarContainer>
     );
 };
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,3 +1,149 @@
 import Sidebar from "./Sidebar.jsx";
+import styled from "styled-components";
 
 export default Sidebar;
+
+export const ImgLogo = styled.img`
+  width: 2rem;
+  height: 2rem;
+  padding-left: 0.37rem;
+`;
+
+export const ToggleSidebarButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2rem;
+  height: 2rem;
+  outline: none;
+  border-radius: 50%;
+  background: var(--color-button-background-${(props) => props.$color}-${(props) => props.$isOpened ? 'active': 'default'});
+  cursor: pointer;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: ${(props) => props.$isOpened ? 'calc(100dvw - 89.35dvw)' : 'calc(100dvw - 94.35dvw)'};
+
+  &:hover {
+    svg {
+      color: var(--color-text-${(props) => props.$color}-hover);
+    }
+  }
+
+ 
+  svg {
+    color: var(--color-text-${(props) => props.$color}-default);
+  }
+`;
+
+export const LinkInnerBox = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  padding: 0.8rem;
+  background: var(--color-sidebar-background-${(props) => props.$color}-default);
+  border-radius: 1rem;
+  height: 1.25rem;
+  width: ${(props) => props.$isOpened ? 'auto': '1.25rem'};
+  cursor: pointer;
+
+  svg {
+    color: var(--color-text-${(props) => props.$color}-default);
+  }
+
+  &:hover {
+    background: var(--color-sidebar-background-${(props) => props.$color}-hover);
+
+    span {
+      color: var(--color-text-${(props) => props.$color}-hover);
+    }
+
+    svg {
+      color: var(--color-text-${(props) => props.$color}-hover);
+    }
+  }
+
+  &.active {
+    background: var(--color-sidebar-background-${(props) => props.$color}-active);
+
+    span {
+      color: var(--color-text-${(props) => props.$color}-active);
+    }
+
+    svg {
+      color: var(--color-text-${(props) => props.$color}-active);
+    }
+  }
+
+`;
+
+export const LinkDesc = styled.span`
+  display: ${(props) => props.$show ? 'inline' : 'none'};
+  margin-left: 1rem;
+  font-size: 1rem;
+  font-weight: 400;
+  color: var(--color-text-${(props) => props.$color}-default);
+`;
+
+export const LogoDesc = styled(LinkDesc)`
+  font-size: 1.25rem;
+  margin-left: 0.75rem;
+  font-weight: 600;
+  color: var(--color-text-logo-${(props) => props.$color}-default);
+
+  &:hover {
+    color: 
+  }
+`;
+
+export const HeaderSection = styled.div`
+  display: flex;
+  align-items: center;
+  height: fit-content;
+  position: relative;
+`;
+
+export const SidebarContainer = styled.div`
+  display: grid;
+  grid-auto-flow: row;
+  width: ${(props) => props.$isOpened ? `calc(100dvw - 90dvw)` : `calc(100dvw - 96.87dvw)`};
+  height: 92dvh;
+  padding: 1.75rem;
+  border-radius: 1.25rem;
+  background: var(--color-sidebar-background-${(props) => props.$color}-default);
+`;
+
+export const NavContainer = styled.nav`
+  display: grid;
+  grid-auto-flow: row;
+  grid-gap: 0.5rem;
+  height: fit-content;
+`;
+
+export const NavBottomContainer = styled(NavContainer)`
+  align-self: end;
+`;
+
+
+// use TS for props type control
+
+/* SidebarContainer.propTypes = {
+  $color: PropTypes.string,
+  $isOpened: PropTypes.boolean,
+}
+
+LinkDesc.propTypes = {
+  $color: PropTypes.string,
+  $show: PropTypes.boolean,
+}
+
+LinkInnerBox.propTypes = {
+  $color: PropTypes.string,
+  $isOpened: PropTypes.boolean,
+}
+
+ToggleSidebarButton.propTypes = {
+  $color: PropTypes.string,
+  $isOpened: PropTypes.boolean,
+} */
